1a42e4e8409618aa78ef13fcf18b246379db6b4d
Ericlecio Thiago <101409162+Ericlecio@users.noreply.github.com>
2024-11-11 21:47:32 -0300

    Enviando.

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Navbar from '../components/Navbar.vue'
 import Footer from '../components/Footer.vue'
+// https://nexus-saude-e8cb6.web.app
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
