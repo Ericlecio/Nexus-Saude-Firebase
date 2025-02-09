09c6b71fb35d280ec01ad0ec5b38a738cea93d7d
Ericlecio <ericleciojr14@gmail.com>
2025-02-09 19:42:05 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Divide dependências em um arquivo separado
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 800, // Ajusta o limite do aviso
+  },
+});
